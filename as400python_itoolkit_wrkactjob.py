c34c7b8ec12724316f61d84ef60b64561fd38fad
khao_lek <khao_lek@yahoo.com>
2023-10-21 14:02:33 +0700

    Create as400python_itoolkit_wrkactjob.py

---
+++ as400python_itoolkit_wrkactjob.py
@@ -0,0 +1,17 @@
+
+from itoolkit import *
+from itoolkit.transport import DatabaseTransport
+import pyodbc
+
+conn = pyodbc.connect("DSN=TPIDEV;uid=quser;pwd=quser")
+itransport = DatabaseTransport(conn)
+itool = iToolKit()
+
+itool.add(iCmd5250('wrkactjob', 'WRKACTJOB'))
+itool.call(itransport)
+wrkactjob = itool.dict_out('wrkactjob')
+
+#print(wrkactjob)
+for key, value in wrkactjob.items():
+	print(f"key > {key}")
+	print(f"value > {value}")
